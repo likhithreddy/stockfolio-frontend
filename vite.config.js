ba2203a80066ce68fada47c0d83dd6f839302077
Likhith Rechintala <rlikhith@gmail.com>
2025-07-30 22:04:42 -0400

    bootstrap cdn implementation

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   // proxy: {
-  //   //   "/login": "http://localhost:8080",
-  //   //   "/admin": "http://localhost:8080",
-  //   // },
-  //   // fs: {
-  //   //   strict: false,
-  //   // },
-  //   // // 👇👇👇 ADD THIS 👇👇👇
-  //   // historyApiFallback: true,
-  // },
 });
